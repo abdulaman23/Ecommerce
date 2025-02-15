572de67a74d971f4624e3cc5f43f08c180ea97bd
Abdul Aman <kshmr.lg@gmail.com>
2025-02-15 16:50:11 +0400

    Configured App and Db details for the app

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce_db',
+        'USER': 'root',
+        'PASSWORD': 'Tgaiznotlawrd@9510',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
